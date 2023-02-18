1f1380d2002578675cf5bb22f877c6f0c4d74b1f
Felipe ALCM <felipealcmagalhaes@gmail.com>
2023-02-18 17:54:30 -0300

    Criação da lógica JS e aplicação no design

---
+++ js/cronometro.js
@@ -0,0 +1,55 @@
+const startButton = document.querySelector("#control-painel .start-button");
+const stopButton = document.querySelector("#control-painel .stop-button");
+const screenContent = document.querySelector("#screen");
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+
+let cron;
+function start() {
+  pause();
+  cron = setInterval(() => {
+    timer();
+  }, 10);
+}
+function pause() {
+  clearInterval(cron);
+}
+function reset() {
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+
+  screenContent.innerText = "00.00.00.000";
+}
+
+function timer() {
+  if ((milliseconds += 10) === 1000) {
+    milliseconds = 0;
+    seconds++;
+  }
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+
+  screenContent.innerText = `${hours}.${minutes}.${seconds}.${milliseconds}`;
+}
+
+startButton.addEventListener("click", (event) => {
+  const value = event.target.innerText;
+  console.log(value);
+  start();
+});
+
+stopButton.addEventListener("click", (event) => {
+  const value = event.target.innerText;
+  pause();
+  console.log(value);
+});
