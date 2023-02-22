db7542f6fc7f7666f4404e268c97e77cf3d15e1f
Felipe ALCM <felipealcmagalhaes@gmail.com>
2023-02-21 21:11:33 -0300

    Ajustando ToDo

--- JS_ForToDo/main.js
+++ JS_ForToDo/main.js
@@ -1,6 +1,7 @@
 const buttonAdd = document.querySelector("#button-task");
 const task = document.querySelector("#task-writer");
 
+let tasks = [];
 function adicionarTarefa(nomeDaTarefa) {
   // Cria um novo elemento HTML para o card
   const taskCard = document.createElement("div");
@@ -19,9 +20,29 @@ function adicionarTarefa(nomeDaTarefa) {
   // Adiciona o card ao container de tarefas
   const taskContainer = document.getElementById("container");
   taskContainer.appendChild(taskCard);
+
+  // Cria um botão para excluir a tarefa
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Excluir";
+  taskCard.appendChild(deleteButton);
+
+  // Adiciona um event listener para o botão de excluir
+  deleteButton.addEventListener("click", function () {
+    taskCard.remove(); // Remove o card do DOM
+  });
+
+  taskButton.addEventListener("click", function () {
+    console.log("ok");
+  });
 }
 
 buttonAdd.addEventListener("click", (event) => {
-  console.log("Hello");
-  adicionarTarefa(task);
+  if (task.value !== "") {
+    adicionarTarefa(task.value);
+    task.value = "";
+  } else {
+    return;
+  }
 });
+
+buttonAdd;
